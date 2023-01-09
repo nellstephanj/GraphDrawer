d4adb6f640246cd53199717ad1e0172372cb8128
Stephan Nell <nellstephanj@gmail.com>
2023-01-09 22:00:38 +0100

    Add example graph

--- graph-drawer/src/app/app.component.ts
+++ graph-drawer/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {multi} from './data';
+import {Color, ScaleType} from "@swimlane/ngx-charts";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graph-drawer';
+
+  multi: any[] = [];
+  view: [number, number] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme: Color = {
+    name: "Line-Drawer",
+    selectable: false,
+    group: ScaleType.Time,
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  constructor() {
+    Object.assign(this, {multi});
+  }
+
+  onSelect(data: any[]): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any[]): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any[]): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
--- graph-drawer/src/app/app.module.ts
+++ graph-drawer/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LineChartModule} from "@swimlane/ngx-charts";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        LineChartModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
