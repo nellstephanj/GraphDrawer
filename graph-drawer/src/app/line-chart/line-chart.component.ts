b787e66b5a1e69d7ddba200d362894e52bebd23e
Stephan Nell <nellstephanj@gmail.com>
2023-01-15 21:59:12 +0100

    Cleanup components

---
+++ graph-drawer/src/app/line-chart/line-chart.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import {Color, ScaleType} from "@swimlane/ngx-charts";
+import {multi} from "../data";
+
+@Component({
+  selector: 'app-line-chart',
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.scss']
+})
+export class LineChartComponent {
+  multi: any[] = [];
+  view: [number, number] = [700, 300];
+
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme: Color = {
+    name: "Line-Drawer",
+    selectable: false,
+    group: ScaleType.Time,
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  constructor() {
+    Object.assign(this, {multi});
+  }
+
+  onSelect(data: any[]): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any[]): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any[]): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
