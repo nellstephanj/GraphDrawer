138479290f660e1ad92d0d56694e1bb5960f96fb
Stephan Nell <nellstephanj@gmail.com>
2023-01-23 18:02:29 +0100

    Cleanup data

--- graph-drawer/src/app/line-chart/line-chart.component.ts
+++ graph-drawer/src/app/line-chart/line-chart.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Color, ScaleType} from "@swimlane/ngx-charts";
 import {SignalRService} from "../core/services/signal-r.service";
 import {GraphValueContent} from "../core/domain/GraphValueContent";
-import {multi} from "../data";
 
 @Component({
   selector: 'app-line-chart',
@@ -14,7 +13,6 @@ export class LineChartComponent implements OnInit {
   view: [number, number] = [700, 300];
   xAxisLabel: string = 'Item';
   yAxisLabel: string = 'Value';
-  counter: number = 99; // TODO: Remove
 
   colorScheme: Color = {
     name: "Line-Drawer",
@@ -53,26 +51,11 @@ export class LineChartComponent implements OnInit {
     });
   }
 
-  // initData() {
-  //   const array = [];
-  //   for (let i = 0; i < 100; i++) {
-  //     array.push({
-  //       "name": i.toString(),
-  //       "value": i
-  //     });
-  //   }
-  //   return array;
-  // }
-
   addData(message: GraphValueContent) {
-    this.counter++;
-    // console.log("test", this.counter, this.multi);
     if(this.multi[0].series.length > 1000){
       this.multi[0].series.shift();
     }
 
-
-    let count = this.multi[0].series.length;
     const data =
       {
         "name": message.yAxis,
